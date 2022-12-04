930a613c2386c932ff2517d62625946d9d7751ff
Jomar Milan <ilovetirzah@gmail.com>
2022-12-04 11:31:27 -0800

    Add example set bonus utilizing ModifyHitByOwnProjectile

---
+++ ExampleMod/Common/Players/ExampleMinerSetBonusPlayer.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExampleMod.Common.Players
+{
+	public class ExampleMinerSetBonusPlayer : ModPlayer
+	{
+		private bool _minerSet;
+
+		public override void PostUpdateEquips() {
+			_minerSet = (Player.head == ArmorIDs.Head.MiningHelmet || Player.head == ArmorIDs.Head.UltraBrightHelmet) &&
+			            Player.body == ArmorIDs.Body.MiningShirt &&
+			            Player.legs == ArmorIDs.Legs.MiningPants;
+		}
+
+		public override void ModifyHitByOwnProjectile(Projectile proj, ref int damage) {
+			if (_minerSet && !proj.trap && !proj.originatedFromActivableTile) {
+				damage = damage / 2;
+			}
+		}
+	}
+}
